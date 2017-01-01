86b8f956485afff48b4e6a9a33246d9dd8619c6a
Italo Lelis de Vietro <italolelis@gmail.com>
2017-01-01 17:59:50 +0100

    Added read and write databases

--- config/config.go
+++ config/config.go
@@ -4,18 +4,23 @@ import "github.com/kelseyhightower/envconfig"
 
 // Specification for basic configurations
 type Specification struct {
-	Port        string `envconfig:"PORT" default:"8080"`
-	LogLevel    string `envconfig:"LOG_LEVEL" default:"info"`
-	DatabaseDSN string `envconfig:"DATABASE_DSN" required:"true"`
+	Port     string `envconfig:"PORT" default:"8080"`
+	LogLevel string `envconfig:"LOG_LEVEL" default:"info"`
+	Database Database
+}
+
+type Database struct {
+	ReadDSN  string `envconfig:"DATABASE_READ_DSN" required:"true"`
+	WriteDSN string `envconfig:"DATABASE_WRITE_DSN" required:"true"`
 }
 
 //LoadEnv loads environment variables
 func LoadEnv() (*Specification, error) {
-	var config Specification
-	err := envconfig.Process("", &config)
+	var c Specification
+	err := envconfig.Process("", &c)
 	if err != nil {
 		return nil, err
 	}
 
-	return &config, nil
+	return &c, nil
 }
